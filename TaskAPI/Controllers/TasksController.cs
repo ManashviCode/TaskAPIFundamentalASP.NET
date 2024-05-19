54c11c074bec402dfaf651f414c26772fb361c5e
Divani Perera <divamanashvi03@gmail.com>
2024-05-19 15:34:16 +0530

    Add project files.

---
+++ TaskAPI/Controllers/TasksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    //Attributes
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]//we have to include like this type of method before the method
+        public IActionResult Tasks() //IActionResult: from this we can return OK from this type of method
+        {
+            var tasks = new String[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks);
+           
+        }
+
+        [HttpPost]
+        public IActionResult NewTasks()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTasks()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTasks()
+        {
+            var SomethingWentWrong = true;
+            if (SomethingWentWrong)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
