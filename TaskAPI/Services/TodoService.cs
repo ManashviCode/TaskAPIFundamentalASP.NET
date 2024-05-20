b69407c7e2444d25dab348c125dc49fa0f11f1b9
Divani Perera <divamanashvi03@gmail.com>
2024-05-20 21:51:15 +0530

    created and separated models and services for new Todos endpoints

---
+++ TaskAPI/Services/TodoService.cs
@@ -0,0 +1,48 @@
+﻿using TaskAPI.Models;
+
+namespace TaskAPI.Services
+{
+    public class TodoService
+    {
+        //Get Todos
+        public List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "Get books for school",
+                Description = "Get some text books for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo1);
+
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "Get vegetables",
+                Description = "Get vegetables for the week",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(2),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo2);
+
+            var todo3 = new Todo
+            {
+                Id = 3,
+                Title = "Water the plans",
+                Description = "Water all the plants quickly",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(1),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo3);
+
+            return todos;
+        }
+    }
+}
